8b87ac1265a2c2aece532e8d52c6a922a4443e75
hoducvinhh <hoducvinh2k4@gmail.com>
2026-09-22 08:57:34 +0700

    feat(email): integrate internalSecretGuard for enhanced security in InternalEmailController

--- src/apps/email/email.module.ts
+++ src/apps/email/email.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { InternalEmailController } from './email.controller';
 import { ConfigModule } from '@nestjs/config';
+import { internalSecretGuard } from 'src/core/guards/internal-secret.guard';
 
 @Module({
   imports: [ConfigModule],
   controllers: [InternalEmailController],
-  providers: [EmailService],
+  providers: [EmailService, internalSecretGuard],
   exports: [EmailService],
 })
 export class EmailModule { }
